7834c01c9069ce5ba61b9ae837263a6639f8e18e
Dan Gilleland <dgilleland@nait.ca>
2024-01-15 09:52:30 -0700

    Added workspace folder + finished 003

--- src/003/HelloWorld/Program.cs
+++ src/003/HelloWorld/Program.cs
@@ -1,9 +1,22 @@
 ﻿namespace App;
+using static System.Console; // Allow me to just type WriteLine() instead of Console.WriteLine()
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        WriteLine("Hello, World!");
+        if (args.Length == 0)
+        {
+            ForegroundColor = ConsoleColor.Red;
+            WriteLine("\tNo information was passed to this program.");
+        }
+        else
+        {
+            ForegroundColor = ConsoleColor.Green;
+            foreach (string info in args)
+                WriteLine($"\t{info}");
+        }
+        ResetColor();
     }
 }
