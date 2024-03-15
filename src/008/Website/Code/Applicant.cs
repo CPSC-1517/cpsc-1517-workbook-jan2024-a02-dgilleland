a3cef2297dca041f3a92d48e5bcef907b576cf95
Dan Gilleland <dgilleland@nait.ca>
2024-03-15 10:55:48 -0600

    WIP 008: Expanding on HTML messages

--- src/008/Website/Code/Applicant.cs
+++ src/008/Website/Code/Applicant.cs
@@ -2,14 +2,14 @@
 
 public class Applicant
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public string Street_1 { get; set; }
-    public string Street_2 { get; set; }
-    public string City { get; set; }
-    public string Province { get; set; }
-    public string PostalCode { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+    public string? Street_1 { get; set; }
+    public string? Street_2 { get; set; }
+    public string? City { get; set; }
+    public string? Province { get; set; }
+    public string? PostalCode { get; set; }
 
     public override string ToString()
     {
