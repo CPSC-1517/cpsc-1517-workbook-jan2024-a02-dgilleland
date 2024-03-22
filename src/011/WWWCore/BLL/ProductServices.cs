201c8b6b0e30ec8ed08935fe7f6bf5ebe241e77b
Dan Gilleland <dgilleland@nait.ca>
2024-03-21 22:54:25 -0600

    Inject Services into page

--- src/011/WWWCore/BLL/ProductServices.cs
+++ src/011/WWWCore/BLL/ProductServices.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Microsoft.EntityFrameworkCore; // for the .Include
 
 namespace WestWindWholesale;
+
 public class ProductServices
 {
     private readonly WestWindContext _context;
